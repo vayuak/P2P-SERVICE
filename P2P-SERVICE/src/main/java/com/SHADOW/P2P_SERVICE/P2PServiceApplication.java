2e361be8ecfbd57932b15bce59a721c429120135
vayuak <adityakakade7350@gmail.com>
2026-08-08 19:58:14 +0530

    Updates

--- P2P-SERVICE/src/main/java/com/SHADOW/P2P_SERVICE/P2PServiceApplication.java
+++ P2P-SERVICE/src/main/java/com/SHADOW/P2P_SERVICE/P2PServiceApplication.java
@@ -2,18 +2,9 @@ package com.SHADOW.P2P_SERVICE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient; // 🟢 ADDED THIS IMPORT
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class,
-        RedisAutoConfiguration.class,
-        RedisRepositoriesAutoConfiguration.class
-})
+@SpringBootApplication
 @EnableDiscoveryClient
 public class P2PServiceApplication {
     public static void main(String[] args) {
